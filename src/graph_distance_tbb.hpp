9df18aee20e68c6c6e22a627b25fcd36e771062e
Mo Mirza <mo.mirza@iwoca.co.uk>
2013-03-01 01:35:41 +0000

    added commnets to graph_distance_tbb

--- src/graph_distance_tbb.hpp
+++ src/graph_distance_tbb.hpp
@@ -89,16 +89,20 @@ std::vector<int> graph_distance_tbb(const std::vector<node> &nodes, int start)
 	std::vector<int> distance(nodes.size(), INT_MAX); // all nodes start at infinite distance
 	
 	// a list of (id,dist)
-	concurrent_queue<std::pair<int,int> > todo; // deque is a mutable double ended queue 
+	concurrent_queue<std::pair<int,int> > todo; 
 	todo.push(std::make_pair(start,0));
 	std::pair<int,int> curr;
 	while(todo.try_pop(curr)){ // loop until todo is empty
+		// maybe this can be a task but does that make sense? makes sense to make for loop
+		// parallel but that doesn't work :(
 		if(distance[curr.first]==INT_MAX){
 			distance[curr.first]=curr.second;
+			// fials with paralllel for and speedup is worse
 			parallel_for(blocked_range<int>(0, nodes[curr.first].edges.size()), PushFor(&todo, nodes, &curr));
-			// for(int i=0;i<nodes[curr.first].edges.size();i++){
-			// 	todo.push(std::make_pair(nodes[curr.first].edges[i],curr.second+1));
-			// }	
+			// the following works but is also slow.
+			for(int i=0;i<nodes[curr.first].edges.size();i++){
+				todo.push(std::make_pair(nodes[curr.first].edges[i],curr.second+1));
+			}	
 		}
 	}
 	
